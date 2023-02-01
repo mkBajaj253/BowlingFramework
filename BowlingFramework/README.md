5eaa5f373d3a64902787413c5f28ba59839766af
mkBajaj253 <maheshm253@gmail.com>
2023-02-01 04:37:58 -0500

    initial setup

---
+++ BowlingFramework/README.md
@@ -0,0 +1,3 @@
+# BowlingFramework
+
+A description of this package.
---
+++ BowlingFramework/Sources/BowlingFramework/BowlingFramework.swift
@@ -0,0 +1,6 @@
+public struct BowlingFramework {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
