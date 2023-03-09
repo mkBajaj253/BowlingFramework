ca8e219002597e1bd6b5c7c761c0dc2e4c50b723
Yves Dukuze <yvesduk06@live.co.uk>
2023-03-09 11:49:16 +0000

    Bowling Alley Frame Work Initial Commit

---
+++ BowlingAlley/BowlingAlley/BowlingAlley.h
@@ -0,0 +1,18 @@
+//
+//  BowlingAlley.h
+//  BowlingAlley
+//
+//  Created by apple on 07/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BowlingAlley.
+FOUNDATION_EXPORT double BowlingAlleyVersionNumber;
+
+//! Project version string for BowlingAlley.
+FOUNDATION_EXPORT const unsigned char BowlingAlleyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowlingAlley/PublicHeader.h>
+
+
